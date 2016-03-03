89dfa76b4422f56673f420d5df862d3df9311b0e
Artur Belousov <g.infosecurity@gmail.com>
2016-03-03 18:22:55 +0200

    work

--- src/main/java/com/pack/CalculatorService.java
+++ src/main/java/com/pack/CalculatorService.java
@@ -1,16 +1,61 @@
 package com.pack;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.pack.request.Request;
+import com.pack.response.Response;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(method = RequestMethod.POST,
+        consumes = "application/json",
+        produces = "application/json")
 public class CalculatorService {
 
-    @RequestMapping("/home")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    @RequestMapping("/sum")
+    Response sum(@RequestBody Request request) {
+        Response response = new Response();
+        Double sum = request.getFirstValue() + request.getSecondValue();
+        response.setResult(getResult(sum));
+        return response;
+    }
+
+    @RequestMapping("/difference")
+    Response difference(@RequestBody Request request) {
+        Response response = new Response();
+        Double difference = request.getFirstValue() - request.getSecondValue();
+        response.setResult(getResult(difference));
+        return response;
+    }
+
+    @RequestMapping("/multiplication")
+    Response multiplication(@RequestBody Request request) {
+        Response response = new Response();
+        Double multiplication = request.getFirstValue() * request.getSecondValue();
+        response.setResult(getResult(multiplication));
+        return response;
+    }
+
+    @RequestMapping("/division")
+    Response division(@RequestBody Request request) {
+        Response response = new Response();
+        Double division = request.getFirstValue() / request.getSecondValue();
+        response.setResult(getResult(division));
+        return response;
+    }
+
+    private String getResult(Double sum) {
+        String responseValue;
+        if (isInteger(sum)) {
+            responseValue = String.valueOf(sum.intValue());
+        } else {
+            responseValue = String.valueOf(sum);
+        }
+        return responseValue;
+    }
+
+    private boolean isInteger(Double sum) {
+        return sum % 1 == 0;
     }
 }
---
+++ src/main/java/com/pack/request/Request.java
@@ -0,0 +1,27 @@
+package com.pack.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Request {
+
+    private Double firstValue;
+    private Double secondValue;
+
+    public Double getFirstValue() {
+        return firstValue;
+    }
+
+    @JsonProperty(value = "first")
+    public void setFirstValue(Double firstValue) {
+        this.firstValue = firstValue;
+    }
+
+    public Double getSecondValue() {
+        return secondValue;
+    }
+
+    @JsonProperty(value = "second")
+    public void setSecondValue(Double secondValue) {
+        this.secondValue = secondValue;
+    }
+}
