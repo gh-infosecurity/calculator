bddd4182142e1b0307deecde6a31a8983a4b9b2e
Artur Belousov <g.infosecurity@gmail.com>
2016-03-09 11:37:48 +0200

    CalculatorConstants add background

---
+++ src/main/java/com/education/calculator/CalculatorConstants.java
@@ -0,0 +1,17 @@
+package com.education.calculator;
+
+public final class CalculatorConstants {
+
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+    public static final String MULTIPLICATION = "*";
+    public static final String DIVISION = "/";
+    public static final String SQRT = "sqrt";
+    public static final String POW = "pow 2";
+    public static final String DILIMITER = " = ";
+    public static final String OPEN_QUOTE = "(";
+    public static final String CLOSE_QUOTE = ")";
+
+    private CalculatorConstants() {
+    }
+}
